99692b4f32b4ca38830bda6618c41603dd12092d
Hoaison3108 <163942895+Hoaison3108@users.noreply.github.com>
2025-06-10 21:30:48 +0700

    Add files via upload

---
+++ Bai22_Object/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai22_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //1. Tạo đối tượng sinh viên mới không truyền giá trị
+            SinhVien sinhVien1 = new SinhVien();
+            SinhVien sinhVien2 = new SinhVien();
+
+            //Xuất thông tin sinh viên
+            Console.WriteLine(sinhVien1.MaSV);
+            Console.WriteLine(sinhVien1.TenSV);
+            Console.WriteLine(sinhVien2.MaSV);
+            Console.WriteLine(sinhVien2.TenSV);
+            Console.WriteLine("Tên sinh viên là: " + sinhVien1.TenSV);
+
+            //2. Tạo đối tượng sinh viên mới truyền giá trị
+            SinhVien sinhVien3 = new SinhVien(3, "Nguyễn Văn A");
+
+            //Xuất thông tin sinh viên 3
+            Console.WriteLine(sinhVien3.MaSV);
+            Console.WriteLine(sinhVien3.TenSV);
+
+            //3. Sửa dữ liệu đối tượng
+            sinhVien3.MaSV = 123;
+            sinhVien3.TenSV = "Nguyễn Văn B";
+
+            Console.WriteLine(sinhVien3.MaSV);
+            Console.WriteLine(sinhVien3.TenSV);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bai22_Object/SinhVien.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai22_Object
+{
+    public class SinhVien
+    {
+        //#khai báo biến lớp
+        private int maSV;
+        private string tenSV;
+
+        //khai báo contructor mặc định
+        //contructor hàm dùng khởi tạo giá trí cho đối tượng
+
+        //#Khai báo contructor
+        //khai báo contructor mặc định
+        public SinhVien()
+        {
+            this.maSV = 0;
+            this.tenSV = "No name";
+        }
+
+        //khai báo contructor có tham số
+        public SinhVien(int maSV, string tenSV)
+        {
+            this.maSV = maSV;
+            this.tenSV = tenSV;
+        }
+
+        //#Khai báo property
+        //khai báo Property để truy xuất sửa dối dữ liệu
+
+        public string TenSV
+        {
+            get { return tenSV; } //get chỉ đọc
+            set { tenSV = value; } //set chỉ ghi
+        }
+
+        public int MaSV
+        {
+            get { return maSV; } //get chỉ đọc
+            set { maSV = value; } //set chỉ ghi
+        }
+
+
+    }
+}
