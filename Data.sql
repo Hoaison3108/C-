ab1eba04003debb69c8e0ff4783ecdd6787c9d6d
Hoaison3108 <163942895+Hoaison3108@users.noreply.github.com>
2025-06-10 21:31:37 +0700

    Add files via upload

---
+++ Data.sql
@@ -0,0 +1,101 @@
+﻿CREATE DATABASE QuanLyQuanCafe
+GO
+
+USE QuanLyQuanCafe
+GO
+
+-- Food
+-- Table
+-- FoodCategory
+-- Account
+-- Bill
+-- BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR (100) NOT NULL DEFAULT N'Bàn chưa có tên',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Trống'  --Trống || có người
+)
+GO
+
+CREATE TABLE Account 
+(
+	UserName NVARCHAR(100) NOT NULL PRIMARY KEY,
+	DisplayName NVARCHAR(100) NOT NULL DEFAULT N'Kter',
+	PassWord NVARCHAR(1000) NOT NULL DEFAULT 0,
+	Type INT NOT NULL DEFAULT 0 -- 1: admin 0: staff
+)
+GO
+
+CREATE TABLE FoodCategory
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR (100) DEFAULT N'Chưa đặt tên',
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR (100) DEFAULT N'Chưa đặt tên',
+	idCategory INT NOT NULL,
+	Price FLOAT NOT NULL DEFAULT 0
+
+	FOREIGN KEY (idCategory) REFERENCES dbo.FoodCategory(id)
+)
+GO
+
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
+	DateCheckOut DATE,
+	idTable INT NOT NULL,
+	Status INT NOT NULL DEFAULT 0 --1: da thanh toan || 0: chua thanh toan
+
+	FOREIGN KEY (id) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idFood INT NOT NULL,
+	COUNT INT NOT NULL DEFAULT 0
+
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
+)
+GO
+
+INSERT INTO dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Type
+)
+VALUES
+(   N'K9', -- UserName - nvarchar(100)
+    N'RongK9', -- DisplayName - nvarchar(100)
+    N'1', -- PassWord - nvarchar(1000)
+    1    -- Type - int
+    )
+
+INSERT INTO dbo.Account
+(
+    UserName,
+    DisplayName,
+    PassWord,
+    Type
+)
+VALUES
+(   N'staff', -- UserName - nvarchar(100)
+    N'staff', -- DisplayName - nvarchar(100)
+    N'1', -- PassWord - nvarchar(1000)
+    0    -- Type - int
+    )
+
+SELECT * FROM dbo.Account
